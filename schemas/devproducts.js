1b579a6bce7defbd289c8794b22b86a57116a8c7
inoaaer <66018796+editlt@users.noreply.github.com>
2024-11-17 11:42:29 -0500

    Fix Bug
    * Description changing from N/A => a blank string should be fixed now.
    * You will most likely need to remove each devProduct causing this error in the DB.

--- schemas/devproducts.js
+++ schemas/devproducts.js
@@ -3,12 +3,12 @@ const { Schema, model } = require("mongoose");
 const devProducts = new Schema({
     ProductId: Number,
     DeveloperProductId: Number,
-    Name: String,
-    Description: String,
-    IconImageAssetId: Number,
-    displayName: String,
-    displayDescription: String,
-    displayIcon: Number,
+    Name: { type: String, default: "N/A" },
+    Description: { type: String, default: "N/A" },
+    IconImageAssetId: { type: Number, default: "1818" },
+    displayName: { type: String, default: "N/A" },
+    displayDescription: { type: String, default: "N/A" },
+    displayIcon: { type: Number, default: "1818" },
     PriceInRobux: Number,
     placeId: Number
 });
